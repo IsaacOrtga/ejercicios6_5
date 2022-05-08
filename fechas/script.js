8f1c729130c1dc705ef860f423101832e0e5f128
IsaacOrtga <ortga.isaac@gmail.com>
2022-05-08 16:09:17 +0200

    fechas actualizado

--- fechas/script.js
+++ fechas/script.js
@@ -1,13 +1,17 @@
-// Una función que devuelva la diferencia en días entre dos fechas del mismo año (sólo tenemos en cuenta dia y mes)
+let day1 = parseInt(prompt("Introduzca un día:"));
+let month1 = parseInt(prompt("Introduzca un día:"));
+let day2 = parseInt(prompt("Introduzca un día:"));
+let month2 = parseInt(prompt("Introduzca un día:"));
+let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+let countD = 0;
+let countM = 0;
 
-function calculate () {
-    let start = new Date (document.getElementById("start").value);
-    let end = new Date (document.getElementById("end").value);
+for(let i = month1; i<month2; i++){
+    countM = countM + months[i];
 
-    var diffDays= end.getTime()-start.getTime();
-
-    var countdays = Math.round(diffDays/(1000*60*60*24));
-
-    alert(countdays);
-    }
-
+}
+countD = countM-(day1-1)+day2;
+if(countD == 1){
+   
+}
+ alert("Faltan "+ countD + " días.")
